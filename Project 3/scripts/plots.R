7124cdd6b1aa4f496a8181840ce56e2714fa430a
monicaeb <54555505+monicaeb@users.noreply.github.com>
2019-11-12 22:13:14 -0500

    python wrangling

---
+++ Project 3/scripts/plots.R
@@ -0,0 +1,11 @@
+##### 
+
+# plots to be used in report and presentation for bios 611 project 3, UMD part 3
+
+library(tools)
+library(tidyverse)
+
+dat <- read.csv("\\.csv",stringsAsFactors=FALSE)
+
+
+
