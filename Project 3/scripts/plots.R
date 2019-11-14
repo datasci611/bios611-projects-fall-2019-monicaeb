884979e60c9ac30d2edc8bca16eb34c714dcc3a5
monicaeb <54555505+monicaeb@users.noreply.github.com>
2019-11-14 18:26:04 -0500

    plots drafted, updated wrangling

--- Project 3/scripts/plots.R
+++ Project 3/scripts/plots.R
@@ -7,7 +7,7 @@ library(tidyverse)
 library(ggmap)
 
 dat <- read.csv("C:\\Users\\Monica Borges\\OneDrive - University of North Carolina at Chapel Hill\\BIOS 611\\GitHub Resources\\bios611-projects-fall-2019-monicaeb\\Project 3\\data\\p3_clean.csv",stringsAsFactors=FALSE)
-
+setwd("C:\\Users\\Monica Borges\\OneDrive - University of North Carolina at Chapel Hill\\BIOS 611\\GitHub Resources\\bios611-projects-fall-2019-monicaeb\\Project 3")
 colnames(dat)
 
 # demog plots: overall, how old are clients? histogram of age
@@ -15,12 +15,14 @@ demog <- unique(dat[,c("Client.ID","Client.Age.at.Entry","Client.Gender","Client
 meanage <- mean(as.numeric(demog$Client.Age.at.Entry),na.rm=TRUE)
 medage <- median(as.numeric(demog$Client.Age.at.Entry),na.rm=TRUE)
 p_agehist <- ggplot(data=demog,aes(x=Client.Age.at.Entry)) +
-  geom_histogram(bins=20,fill="snow3",color="skyblue4") +
+  geom_histogram(bins=20,fill="snow2",color="skyblue4") +
   theme_minimal() +
-  geom_vline(aes(xintercept=meanage),color="darkgreen") + # mean = 44.3
-  geom_vline(aes(xintercept=medage),color="seagreen") + # med = 46, n=3 are missing
+  geom_vline(aes(xintercept=meanage),color="indianred3",size=1) + # mean = 44.3
+  geom_vline(aes(xintercept=medage),color="indianred4",size=1) + # med = 46, n=3 are missing
   labs(title="Figure 1. Histogram of Client Age Upon Arrival",x="Age (years)",y="Count")
 p_agehist
+
+ggsave(plot=p_agehist,filename="./report/p_agehist.png",device="png")
 
 # increase in people staying, by year, lines by gender and total
 yearinc <- dat %>% group_by(first_year,Client.Gender) %>% mutate(newIDs=length(unique(Client.ID))) %>%
@@ -31,22 +33,24 @@ yearinc.all <- dat %>% group_by(first_year) %>% mutate(newIDs=length(unique(Clie
   mutate(gender="All") %>% distinct()
 yearinc <- data.frame(rbind(yearinc,yearinc.all)) %>% mutate(Gender=factor(gender,levels=unique(gender)))
 
-
 p_yeargend <- ggplot(yearinc,aes(x=first_year)) + 
   geom_point(aes(y=newIDs, col=Gender)) +
   geom_smooth(aes(y=newIDs, col=Gender),se=FALSE) +
-  theme_minimal() +
+  theme_minimal() + scale_color_brewer(palette = "Set2") +
   labs(title="Figure 2. Number of New Clients per Year by Gender",x="Year",y="# Of Clients Arriving")
 p_yeargend
+
+ggsave(plot=p_yeargend,filename="./report/p_yeargend.png",device="png")
+
 
 # histograms of people arriving/leaving, split by month
 f_visit <- dat %>% group_by(first_year,first_m) %>% mutate(sumIDs=length(unique(Client.ID))) %>%
   ungroup() %>% select(first_year,first_m,sumIDs) %>%
-  mutate(first_c=month.abb[first_m]) %>% 
+  mutate(first_c=month.abb[first_m]) %>% filter(!is.na(first_c)) %>%
   mutate(first_c=factor(first_c,levels=c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")))
 l_visit <- dat %>% group_by(last_year,last_m) %>% mutate(sumIDs=length(unique(Client.ID))) %>%
   ungroup() %>% select(last_year,last_m,sumIDs) %>%
-  mutate(last_c=month.abb[last_m]) %>%
+  mutate(last_c=month.abb[last_m]) %>% filter(!is.na(last_c)) %>%
   mutate(last_c=factor(last_c,levels=c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")))
 
 p_histf <- ggplot(data=f_visit,aes(x=first_c,y=sumIDs)) +
@@ -62,6 +66,10 @@ p_histl <- ggplot(data=l_visit,aes(x=last_c,y=sumIDs)) +
   theme_minimal() +
   labs(title="Figure 4: Distribution of Number of Departures by Month")
 p_histl
+
+ggsave(plot=p_histf,filename="./report/p_histf.png",device="png")
+ggsave(plot=p_histl,filename="./report/p_histl.png",device="png")
+
 
 # box plot length of stay at UMD by housing status
 dat$stat <- dat$Housing.Status.2703.
@@ -81,24 +89,32 @@ p_length <- ggplot(data=dat.stat[dat.stat$diffdays<=179,]) +
   #geom_point(aes(x=status,y=diffdays),color="snow4",size=0.9) + 
   #geom_jitter(aes(x=status,y=diffdays),width=0.25,color="snow4",size=0.3) +
   theme_minimal() + theme(axis.text.y = element_text(angle = 30, hjust = 1)) +
-  labs(title="Figure 4: Days spent at UMD Shelter (up to 95% quantile) by Housing Status",y="Days",x="Housing\nStatus")
+  labs(title="Figure 5: Days spent at UMD Shelter (up to 95% quantile) by Housing Status",y="Days",x="Housing\nStatus")
 p_length
+
+ggsave(plot=p_length,filename="./report/p_length.png",device="png")
+
 
 ## housing status by destination
 unique(dat$Destination)
 dat.dest <- dat %>%
-  mutate(dest=ifelse(grepl(", permanent tenure",Destination),"friends/fam/perm",
-                     ifelse(grepl(", temporary tenure",Destination),"friends/fam/temp",
-                            ifelse(grepl("Rental|rental",Destination),"rental",
-                                   ifelse(grepl("psychiatric|abuse|care facility|Hospital",Destination),"hosp/psych/care facility",
-                                          ifelse(grepl("Owned by client",Destination),"owned",
-                                                 ifelse(grepl("project|shelter|Transitional|formerly homeless persons",Destination),"shelter/proj/trans/hotel",
-                                                        ifelse(grepl("Jail",Destination),"jail","Other/Unknown")))))))) %>%
-  select(Client.ID,dest,status,diffdays) %>% filter(dest!="",status!="")
+  mutate(dest=ifelse(grepl(", permanent tenure",Destination),"Friends/family, permanent",
+                     ifelse(grepl(", temporary tenure",Destination),"Friends/family, temp",
+                            ifelse(grepl("Rental|rental",Destination),"Rental",
+                                   ifelse(grepl("psychiatric|abuse|care facility|Hospital",Destination),"Hospital/psych/care facility",
+                                          ifelse(grepl("Owned by client",Destination),"Owned",
+                                                 ifelse(grepl("project|shelter|Transitional|formerly homeless persons",Destination),"Shelter/project/transitional",
+                                                        ifelse(grepl("Jail",Destination),"Jail","Unknown")))))))) %>%
+  select(Client.ID,dest,status,diffdays) %>% filter(dest!="",status!="")  %>%
+  mutate(dest=factor(dest,levels=c("Rental","Owned","Friends/family, temp","Friends/family, permanent","Hospital/psych/care facility","Shelter/project/transitional","Unknown", "Jail"))) %>% filter(!is.na(dest))
 
 p_statdest <- ggplot(data=dat.dest, aes(status,fill=dest)) + 
-  geom_bar(position="fill") + scale_color_brewer(palette = "Set2")
+  geom_bar(position="fill") + scale_fill_brewer(palette = "Set2") +
+  labs(title="Figure 6: Destination after UMD by Housing Status at Entry",x="Housing status",y="% of Clients") +
+  guides(fill=guide_legend(title="Next destination"))
 p_statdest
+
+ggsave(plot=p_statdest,filename="./report/p_statdest.png",device="png")
 
 
 
