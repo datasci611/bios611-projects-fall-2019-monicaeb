ae8807ff532f86eead30c8eaf741e6d9df3d5148
monicaeb <54555505+monicaeb@users.noreply.github.com>
2019-11-14 20:50:56 -0500

    plots drafted

--- Project 3/scripts/plots.R
+++ Project 3/scripts/plots.R
@@ -25,10 +25,10 @@ p_agehist
 ggsave(plot=p_agehist,filename="./report/p_agehist.png",device="png")
 
 # increase in people staying, by year, lines by gender and total
-yearinc <- dat %>% group_by(first_year,Client.Gender) %>% mutate(newIDs=length(unique(Client.ID))) %>%
+yearinc <- dat %>% group_by(first_year,Client.Gender) %>% mutate(newIDs=length(Client.ID)) %>%
   ungroup() %>% select(Client.Gender,first_year,newIDs) %>% filter(!is.na(Client.Gender) & Client.Gender!="") %>%
   mutate(gender=gsub(" or Male to Female","",Client.Gender)) %>% distinct()
-yearinc.all <- dat %>% group_by(first_year) %>% mutate(newIDs=length(unique(Client.ID))) %>%
+yearinc.all <- dat %>% group_by(first_year) %>% mutate(newIDs=length(Client.ID)) %>%
   ungroup() %>% select(Client.Gender,first_year,newIDs) %>%
   mutate(gender="All") %>% distinct()
 yearinc <- data.frame(rbind(yearinc,yearinc.all)) %>% mutate(Gender=factor(gender,levels=unique(gender)))
@@ -44,11 +44,11 @@ ggsave(plot=p_yeargend,filename="./report/p_yeargend.png",device="png")
 
 
 # histograms of people arriving/leaving, split by month
-f_visit <- dat %>% group_by(first_year,first_m) %>% mutate(sumIDs=length(unique(Client.ID))) %>%
+f_visit <- dat %>% group_by(first_year,first_m) %>% mutate(sumIDs=length(Client.ID)) %>%
   ungroup() %>% select(first_year,first_m,sumIDs) %>%
   mutate(first_c=month.abb[first_m]) %>% filter(!is.na(first_c)) %>%
   mutate(first_c=factor(first_c,levels=c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")))
-l_visit <- dat %>% group_by(last_year,last_m) %>% mutate(sumIDs=length(unique(Client.ID))) %>%
+l_visit <- dat %>% group_by(last_year,last_m) %>% mutate(sumIDs=length(Client.ID)) %>%
   ungroup() %>% select(last_year,last_m,sumIDs) %>%
   mutate(last_c=month.abb[last_m]) %>% filter(!is.na(last_c)) %>%
   mutate(last_c=factor(last_c,levels=c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")))
@@ -70,8 +70,35 @@ p_histl
 ggsave(plot=p_histf,filename="./report/p_histf.png",device="png")
 ggsave(plot=p_histl,filename="./report/p_histl.png",device="png")
 
+## is there any association between length of stay in previous place and stay at UMD?
+unique(dat$Length.of.Stay.in.Previous.Place.1934.)
 
-# box plot length of stay at UMD by housing status
+dat <- dat %>% mutate(prevstay=Length.of.Stay.in.Previous.Place.1934.) %>%
+  mutate(prevstay=ifelse(prevstay%in%c("Client doesn't know (HUD)", "Client refused (HUD)","Data not collected (HUD)"),"Unknown",
+                         ifelse(prevstay%in%c("One night or less","One week or less (HUD)","Two to six nights"),"< 1 week",
+                                ifelse(prevstay=="One week or more, but less than one month","1 week - 1 month",
+                                       ifelse(prevstay=="One month or more, but less than 90 days","1 - 3 months",
+                                              ifelse(prevstay=="90 days or more, but less than one year","3 months - 1 year",
+                                                     ifelse(prevstay=="One year or longer (HUD)","1+ year","")))))))
+
+dat.prevlen <- dat %>% select(prevstay,diffdays) %>% filter(!is.na(prevstay)) %>%
+  filter(prevstay!="",!is.na(diffdays)) %>% distinct()
+
+# plot up to 0.95 quantile because time spent is so skewed right
+quantile(dat$diffdays,0.95,na.rm=TRUE)
+quantile(dat$diffdays,0.99,na.rm=TRUE)
+
+p_prevlen <- ggplot(data=dat.prevlen[dat.prevlen$diffdays<=303,],aes(x=prevstay,y=diffdays)) +
+  geom_boxplot(color="rosybrown4",fill="seashell1") + 
+  theme_minimal() + theme(axis.text.y = element_text(angle = 30, hjust = 1)) + coord_flip() +
+  labs(title="Figure 5: Days Spent at UMD Shelter (up to 95% quantile)\n by Time in Previous Location",
+       x="Time spent at previous place",y="Days spent at UMD shelter")
+p_prevlen
+
+ggsave(plot=p_statdest,filename="./report/p_statdest.png",device="png")
+
+
+ # box plot length of stay at UMD by housing status
 dat$stat <- dat$Housing.Status.2703.
 dat <- dat %>% mutate(status=ifelse(stat=="At-risk of homelessness (HUD)","At Risk",
                                     ifelse(stat%in%c("Category 1 - Homeless (HUD)","Category 3 - Homeless only under other federal statutes (HUD)"),"Homeless",
@@ -82,18 +109,15 @@ dat <- dat %>% mutate(status=ifelse(stat=="At-risk of homelessness (HUD)","At Ri
 dat.stat <- dat %>% select(status,diffdays,Client.ID,Client.Gender) %>% distinct() %>% 
   filter(!is.na(diffdays),diffdays!="") %>% filter(!is.na(status),status!="") 
 
-quantile(dat.stat$diffdays,0.95)
-
-p_length <- ggplot(data=dat.stat[dat.stat$diffdays<=179,]) +
+p_lengthstat <- ggplot(data=dat.stat[dat.stat$diffdays<=303,]) +
   geom_boxplot(aes(x=status,y=diffdays),color="snow4",fill="lavenderblush") + coord_flip() +
   #geom_point(aes(x=status,y=diffdays),color="snow4",size=0.9) + 
   #geom_jitter(aes(x=status,y=diffdays),width=0.25,color="snow4",size=0.3) +
   theme_minimal() + theme(axis.text.y = element_text(angle = 30, hjust = 1)) +
-  labs(title="Figure 5: Days spent at UMD Shelter (up to 95% quantile) by Housing Status",y="Days",x="Housing\nStatus")
-p_length
+  labs(title="Figure 6: Days spent at UMD Shelter (up to 95% quantile)\n by Housing Status",y="Days",x="Housing\nStatus")
+p_lengthstat
 
-ggsave(plot=p_length,filename="./report/p_length.png",device="png")
-
+ggsave(plot=p_lengthstat,filename="./report/p_lengthstat.png",device="png")
 
 ## housing status by destination
 unique(dat$Destination)
@@ -110,7 +134,7 @@ dat.dest <- dat %>%
 
 p_statdest <- ggplot(data=dat.dest, aes(status,fill=dest)) + 
   geom_bar(position="fill") + scale_fill_brewer(palette = "Set2") +
-  labs(title="Figure 6: Destination after UMD by Housing Status at Entry",x="Housing status",y="% of Clients") +
+  labs(title="Figure 7: Destination after UMD by Housing Status at Entry",x="Housing status",y="% of Clients") +
   guides(fill=guide_legend(title="Next destination"))
 p_statdest
 
